09966b1760d000b099c904e2777ba5a71de2a584
Nataliia <majatrof@gmail.com>
2022-12-27 22:30:52 +0100

    finally commit

--- src/modal.js
+++ src/modal.js
@@ -5,8 +5,8 @@ const objModal = {
   'about-card__read-more': '.modal-readmore',
   'site-button__buy': '.modal-buy-now',
   'product__button': '.modal',
-  'footer-button__submit footer-button__submit--but1': '.modal-contacts',
-  'footer-button__submit footer-button__submit--but2': '.modal-location'
+  'footer-button__submit footer-button__submit--but2': '.modal-contacts',
+  'footer-button__submit footer-button__submit--but1': '.modal-location'
 };
 
 document.addEventListener('click', e => {
@@ -18,7 +18,8 @@ document.addEventListener('click', e => {
     modalBackdrop.classList.remove('is-hidden');
   }
 
-  console.log(e.target.classList.value)
+  console.log(e.target.classList.value);
+
   if (
     e.target.matches('.backdrop') ||
     e.target.classList.value.includes('close')
