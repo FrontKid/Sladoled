c4fdc1abb4f5c7efb21d89be708b9de8748d0657
Nataliia <majatrof@gmail.com>
2022-12-27 17:36:08 +0100

    . додала модалку з локацією. в модалці контакти поміняла стилі
    додала модалку з локацією. в модалці контакти поміняла стилі на правильні назви. js підключити не змогла, прошу допомогти. і картинку локації треба поправити.

--- src/modal.js
+++ src/modal.js
@@ -4,7 +4,9 @@ let currentModal = '';
 const objModal = {
   'about-card__read-more': '.modal-readmore',
   'site-button__buy': '.modal-buy-now',
-  '.product__button': '.modal',
+  'product__button': '.modal',
+  'footer-button__submit--but2': '.modal-contacts',
+  'footer-button__submit--but1': '.modal-location'
 };
 
 document.addEventListener('click', e => {
